9561533c0a6a92be945b3ddd0f708ca6a8784f29
Lursun <lursun914013@gmail.com>
2017-07-28 18:42:23 +0800

    update

--- homework.sol
+++ homework.sol
@@ -1,23 +1,23 @@
 pragma solidity ^0.4.13;
 
 contract jianfe {
-    int8 public targetWeight = 0;
-    int8 public lastWeight = 0;
-    uint256 public amount = 0;
-    address public initiator;
-    uint256 public time = 0;
-    uint16 public goodersCount = 1;
-    uint16 public badersCount = 0;
-    uint256 public goodAmount = 0;
-    uint256 public badAmount = 0;
-    mapping (uint => address) public gooder;
-    mapping (uint => address) public bader;
-    mapping (address => uint256) public goodProportion;
-    mapping (address => uint256) public badProportion;
-    event act(address people,uint256 time);
-    event settleEvent(uint256 amount,uint256 time,string);
+    int8 public targetWeight = 0; //目標體重
+    int8 public lastWeight = 0; //最後一次記錄體重
+    uint256 public amount = 0; //累計總額
+    address public initiator; //發起人
+    uint256 public time = 0; //到期時間
+    uint16 public goodersCount = 1; //看好人數
+    uint16 public badersCount = 0; //看壞人數
+    uint256 public goodAmount = 0; //看好總數
+    uint256 public badAmount = 0; //看壞總數
+    mapping (uint => address) public gooder; //看好人
+    mapping (uint => address) public bader; //看壞人
+    mapping (address => uint256) public goodProportion; //看好個人總數
+    mapping (address => uint256) public badProportion; //看壞個人總數
+    event act(address people,uint256 time); //動作事件
+    event settleEvent(uint256 amount,uint256 time,string); //結算事件
 
-    modifier timeout() { 
+    modifier timeout() { //判斷是否超時 => 結算
         act(msg.sender,now);
         if(now <= time * 1 seconds) {
         _;
@@ -28,7 +28,7 @@ contract jianfe {
             settle();
         }
     }
-    modifier toRaise() { 
+    modifier toRaise() { //判斷是否為發起人=>否則累計總額
         if(msg.sender != initiator) {
             amount += msg.value;
             _;
@@ -41,12 +41,13 @@ contract jianfe {
         time = now + _time;
 
         amount = msg.value;
+        goodAmount = msg.value;
         gooder[0] = initiator;
         goodProportion[initiator] = msg.value;
 
     }
 
-    function good() timeout toRaise payable {
+    function good() timeout toRaise payable { //看好
         goodAmount += msg.value;
         if(goodProportion[msg.sender] > 0) {
             goodProportion[msg.sender] += msg.value;
@@ -57,7 +58,7 @@ contract jianfe {
         }
     }
 
-    function bad() timeout toRaise payable { 
+    function bad() timeout toRaise payable { //看壞
         badAmount += msg.value;
         if(badProportion[msg.sender] > 0) {
             badProportion[msg.sender] += msg.value;
@@ -68,7 +69,7 @@ contract jianfe {
         }
     }
 
-    function recordWeight(int8 _lastWeight) timeout { 
+    function recordWeight(int8 _lastWeight) timeout { //記錄體重
         if(initiator == msg.sender) {
             lastWeight = _lastWeight;
             if (lastWeight <= targetWeight) {
@@ -77,7 +78,7 @@ contract jianfe {
         }
     }
     
-    function settle() { 
+    function settle() { //結算
         if(now >= time * 1 seconds || initiator == msg.sender) {
             if (lastWeight <= targetWeight || badAmount == 0) {
                 settleEvent(amount,now, "good");
@@ -97,16 +98,18 @@ contract jianfe {
 
 contract biyezhuanti {
     mapping (address => address) public contractMap;
-    event newContract(address jianfeAddress ,uint256 money ,uint time);
+    event createContractEvent(address jianfeAddress ,uint256 money ,uint time);
+    event deleteContractEvent(address jianfeAddress ,uint256 money ,uint time);
 
-    function createContract(int8 _targetWeight ,int8 _lastWeight ,uint _time) payable {
+    function createContract(int8 _targetWeight ,int8 _lastWeight ,uint _time) payable { //建立合約
         if( _targetWeight != 0 &&  msg.value != 0) {
             contractMap[msg.sender] = (new jianfe).value( msg.value )(msg.sender ,_targetWeight , _lastWeight , _time ) ;
-            newContract( contractMap[msg.sender] ,msg.value , _time);
+            createContractEvent( contractMap[msg.sender] ,msg.value , _time);
         }
     }
 
-    function deleteContract(address sender) {
+    function deleteContract(address sender) { //刪除合約
+        jianfe(contractMap[sender]).settle() ;
         delete contractMap[sender];
     }
 }
