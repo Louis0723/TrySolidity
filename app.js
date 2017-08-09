b70da21608aff4ce9c8624491fa91a228a07f84a
Lursun <lursun914013@gmail.com>
2017-08-09 22:58:49 +0800

    update

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ let loginCheck=function(req,res){
 }
 let getWeb3=function(){
     let Web3 = require("web3");
+    Web3.prototype.getAccount=function(address,...password){
+        if(password.length==0) password[0]=""
+        this.personal.unlockAccount(address,password[0]);
+        return address
+    }
     let web3 = new Web3();
     web3.setProvider(new web3.providers.HttpProvider(ethereumUri));
+    return web3;
 }
 module.exports=function(app){
     app.get(/^\/$/, function(req, res) {
@@ -31,8 +37,12 @@ module.exports=function(app){
         res.send(newAccount);
     })
     app.get(/^\/createContract/, function(req, res) {
-        if (loginCheck(req,res)) return
+        if (loginCheck(req,res)) return;
         let web3=getWeb3();
+        let biyezhuanti=web3.eth.contract([{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"contractMap","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_targetWeight","type":"int8"},{"name":"_lastWeight","type":"int8"},{"name":"_time","type":"uint256"}],"name":"createContract","outputs":[],"payable":true,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"jianfeAddress","type":"address"},{"indexed":false,"name":"money","type":"uint256"},{"indexed":false,"name":"time","type":"uint256"}],"name":"createContractEvent","type":"event"}],"0xDcbe17a8B781304e28623cc82A68794675548a09");
+        let biyeContract=biyezhuanti.at("0xDcbe17a8B781304e28623cc82A68794675548a09");
+        data=biyeContract.createContract.getData(50,60,3000);
+        web3.eth.sendTransaction({to:"0xDcbe17a8B781304e28623cc82A68794675548a09",from:web3.getAccount("0x002915BdFBDf6C789aa654aaA22E6865D9111F6C"),data:data,value:50000000000000000000,gas:5999999})
         res.send('return contractID');
     })
     app.get(/^\/good/, function(req, res) {
@@ -45,8 +55,7 @@ module.exports=function(app){
         console.log("bad")
         res.send('bad');
     })
-    app.get(/^\/recordWeight/),
-    function(req, res) {
+    app.get(/^\/recordWeight/),function(req, res) {
         if (loginCheck(req,res)) return
         console.log("recordWeight")
         res.send('recordWeight');
--- index.js
+++ index.js
@@ -7,5 +7,5 @@ rount(app)
 let server = app.listen(8081, function() {
     let host = server.address().address
     let port = server.address().port
-    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+    console.log("http://%s:%s", host, port)
 })
